11ade046512e1ea13fb79cefd0e3d1db29e098e6
Edward Chernyavsky <e.chernyavsky@itransition.com>
2016-03-04 17:14:41 +0300

    Initial Commit.

---
+++ ConnectionWithSSRS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConnectionWithSSRS.Startup))]
+namespace ConnectionWithSSRS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
