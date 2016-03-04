085851207e2e52238b21af7c4552eee42ef01c04
Edward Chernyavsky <e.chernyavsky@itransition.com>
2016-03-04 17:28:48 +0300

    Setup identity

--- ConnectionWithSSRS/Models/IdentityModels.cs
+++ ConnectionWithSSRS/Models/IdentityModels.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using ConnectionWithSSRS.Migrations;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -9,6 +10,8 @@ namespace ConnectionWithSSRS.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public bool HasAccessToService { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -23,6 +26,7 @@ namespace ConnectionWithSSRS.Models
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
         }
 
         public static ApplicationDbContext Create()
